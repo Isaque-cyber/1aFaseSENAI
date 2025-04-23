71dea722a14f991cc5bf261fad713759764087fe
ISAQUE RIBEIRO DA SILVA <isaque_r_silva@estudante.sesisenai.org.br>
2025-04-22 21:52:42 -0300

    Calculadora básica

---
+++ html aula 3.html/aula 3.js
@@ -0,0 +1,28 @@
+function mostrarSoma (){
+    let num1 = Number(prompt("Digite um número:"))
+    let num2 = Number(prompt("Digite outro número:"))
+
+    let soma = (num1 + num2)
+alert("A soma dos dois números é:"+ soma)
+} 
+
+
+function mostrarMédia (){
+    let num1 = Number(prompt("Digite um número:"))
+    let num2 = Number(prompt("Digite outro número:"))
+
+    let média = (num1 + num2)/2
+alert("A média é:"+ média)
+} 
+
+
+function mostrarMulti (){
+    let num1 = Number(prompt("Digite um número:"))
+    let num2 = Number(prompt("Digite outro número:"))
+
+    let multi = (num1 * num2)
+alert("A multiplicação dos dois números é:"+ multi)
+} 
+
+
+
