ab3310e0a879e31ec80dfb964ccefe57b2514ab9
ISAQUE RIBEIRO DA SILVA <isaque_r_silva@estudante.sesisenai.org.br>
2025-05-29 22:17:41 -0300

    tela de login

--- testes de css/Página 4/script4.js
+++ testes de css/Página 4/script4.js
@@ -1,16 +1,76 @@
+inicialiar()
+
+
+// let nome, email, senha
+let usuario = {
+    nome: "um",
+    email: "dois",
+    senha: "nuvem"
+}
+function Logar() {
+    let nome = document.getElementById('Lnome').value
+    let senha = document.getElementById('Lsenha').value
+    if (nome === usuario.nome && senha === usuario.senha) {
+        alert("Logado com sucesso")
+    } else {
+        alert("Tentativa de login fracassada")
+    }
+    mostrarProdutos()
+
+}
+
+function cadastrar() {
+    usuario.nome = document.getElementById('Cnome').value
+    usuario.email = document.getElementById('Cemail').value
+    usuario.senha = document.getElementById('Csenha').value
+
+
+    alert("Cadastrado com sucesso")
+
+    mostrarLogin()
+}
+
+function limparCadastro() {
+    document.getElementById("Cnome").value = " "
+    document.getElementById("Cemail").value = " "
+    document.getElementById("Csenha").value = " "
+
+    document.getElementById("Lnome").value = " "
+    document.getElementById("Lsenha").value = " "
+}
+
 
 function mostrarLogin() {
+    esconderTodas()
     document.getElementById("login").style.display = "flex"
-    document.getElementById("cadastro").style.display = "none"
+    document.getElementById("Lnome").focus()
+
 }
 
 function mostrarCadastro() {
-    document.getElementById("login").style.display = "none"
+    esconderTodas()
     document.getElementById("cadastro").style.display = "flex"
+    
 
 }
-function Logar() {
-    // Coleta os valores dos campos de login
-    const email = document.getElementById("name").value;
-    const senha = document.getElementById("senhaLogin").value;
+function mostrarProdutos() {
+    esconderTodas()
+
+    document.getElementById("produtos").style.display = "flex"
+
 }
+
+function esconderTodas() {
+
+    document.getElementById("login").style.display = "none"
+    document.getElementById("cadastro").style.display = "none"
+    document.getElementById("produtos").style.display = "none"
+
+}
+
+function inicializar(){
+    mostrarLogin()
+}
+
+
+// window.open para abrir outro arquivo html
