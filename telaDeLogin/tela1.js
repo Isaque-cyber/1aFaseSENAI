6e8581aa1dcca23f26223f0c2427299702e15790
Isaque-Cyber <isaque_r_silva@estudante.sesisenai.org.br>
2025-05-31 19:18:46 -0300

    new

--- telaDeLogin/tela1.js
+++ telaDeLogin/tela1.js
@@ -1,9 +1,76 @@
+inicializar()
 
-function mostrarLogin(){
-    document.getElementById('login').style.display = 'flex'
-    document.getElementById('cadastro').style.display = 'none'
+
+
+
+let usuario = {
+    nome: '',
+    email: '',
+    senha: ''
 }
-function mostrarCadastro(){
-    document.getElementById('login').style.display = 'none'
-    document.getElementById('cadastro').style.display = 'flex'
+function cadastrar() {
+    usuario.nome = document.getElementById('Cnome').value
+    usuario.email = document.getElementById('Cemail').value
+    usuario.senha = document.getElementById('Csenha').value
+    alert("Cadastrado com sucesso!! :D")
+
+    console.log(usuario)
+
+    limparInputs()
+    mostrarLogin()
+
 }
+
+function logar() {
+    let nome = document.getElementById('Lnome').value
+    let senha = document.getElementById('Lsenha').value
+
+    if ((nome === usuario.nome || nome === usuario.email) && senha === usuario.senha) {
+        alert("Login efetuado com sucesso!")
+        limparInputs()
+        mostrarProdutos()
+        // document.getElementById('navbar').style.display = 'block'
+    } else {
+        alert("Login não efetuado sem sucesso!")
+    }
+}
+
+function mostrarLogin() {
+    esconderBotoes()
+    document.getElementById('Login').style.display = 'flex'
+    document.getElementById('Lnome').focus()
+
+
+}
+function mostrarCadastro() {
+    esconderBotoes()
+    document.getElementById('Cadastro').style.display = 'flex'
+    document.getElementById('Cnome').focus()
+
+}
+function mostrarProdutos() {
+    esconderBotoes()
+    document.getElementById('Produtos').style.display = 'flex'
+    document.getElementById('Pprodutos').focus()
+}
+
+function esconderBotoes() {
+    document.getElementById('Login').style.display = 'none'
+    document.getElementById('Cadastro').style.display = 'none'
+    document.getElementById('Produtos').style.display = 'none'
+}
+
+function limparInputs() {
+    document.getElementById('Cnome').value = ''
+    document.getElementById('Cemail').value = ''
+    document.getElementById('Csenha').value = ''
+
+    document.getElementById('Lnome').value = ''
+    document.getElementById('Lsenha').value = ''
+}
+
+function inicializar() {
+    mostrarLogin()
+
+}
+
