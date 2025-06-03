59d7f3c9871036eb0b6b40e520fb8757055cf29d
Isaque-Cyber <isaque_r_silva@estudante.sesisenai.org.br>
2025-06-03 14:37:08 -0300

    atv 2 do Beecrowd

--- beecrowd com js/script1.js
+++ beecrowd com js/script1.js
@@ -11,5 +11,20 @@ let peso1 = 3.5
 let peso2 = 7.5
 
 let media = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2)
+alert("MÉDIA = " + media)
+}
+
+
+
+function media2(){
+let nota1 = Number(prompt("Digite a primeira nota: "))
+let nota2 = Number(prompt("Digite a segunda nota: "))
+let nota3 = Number(prompt("Digite a terceira nota: "))
+let peso1 = 2.0 
+let peso2 = 3.0
+let peso3 = 5.0
+
+let media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
 alert("MÉDIA = " + media.toFixed(1))
+
 }
