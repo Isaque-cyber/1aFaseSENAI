bf9b03c262c61f6224d9fdc09f294c8197090da5
Isaque-Cyber <isaque_r_silva@estudante.sesisenai.org.br>
2025-07-09 18:00:45 -0300

    Prova de rec

--- beecrowd com js/script1.js
+++ beecrowd com js/script1.js
@@ -49,3 +49,19 @@ function salarioComBonus() {
 
     alert("O salário total do funcionário " + nomeFuncionario + " é " + salarioTotal.toFixed(2) + " reais.")
 }
+
+
+
+// Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+// Entrada
+// O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+// Saída
+// A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.
+function calculoSimples(){
+    let  codigoP1 = prompt("Digite o código de sua peça: ")  //é a numeração da peça
+    let  numeroDP1 = prompt("Digite o número de peças 1: ")  // é a quantidade de peças 1
+    let valorUniP1 = prompt("Digite o valor unitário de cada peça 1: ") // Valor por unidade
+
+
+
+}
